b6679f05ed2055517de5f41635bc3a585ee73262
chayut <chayut@dek-d.com>
2019-07-04 17:57:43 +0700

    Lib Update.

--- app/src/main/java/com/example/chayut/test/RecyclerViewType.kt
+++ app/src/main/java/com/example/chayut/test/RecyclerViewType.kt
@@ -1,7 +1,5 @@
 package com.example.chayut.test
 
-import com.example.chayut.test.BaseRecyclerViewItem
-
 class RecyclerViewType (type : Int) : BaseRecyclerViewItem(type){
     companion object {
         const val TYPE_STICKER = 1
